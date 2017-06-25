812b28a6d72b9a5c053f2a6814371aa28b3ed198
kaunjovi <kaunjovi@gmail.com>
2017-06-25 20:05:39 +0530

    will it build on heroku?

---
+++ src/main/java/fun/n/games/FetchQSEData.java
@@ -0,0 +1,15 @@
+package fun.n.games;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FetchQSEData {
+
+	private static final Logger log = LoggerFactory.getLogger(FetchQSEData.class);
+
+	public static void main(String[] args) {
+		log.debug("Hello world.");
+
+	}
+
+}
