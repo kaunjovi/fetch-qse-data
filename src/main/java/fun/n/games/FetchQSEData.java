56b4438c2f1f99a059639ee7ea14a453d6047856
kaunjovi <kaunjovi@gmail.com>
2017-06-25 21:45:13 +0530

    can we pick JSON data from outside.

--- src/main/java/fun/n/games/FetchQSEData.java
+++ src/main/java/fun/n/games/FetchQSEData.java
@@ -1,5 +1,12 @@
 package fun.n.games;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,8 +14,43 @@ public class FetchQSEData {
 
 	private static final Logger log = LoggerFactory.getLogger(FetchQSEData.class);
 
+	private static final String REST_URL = "http://services.groupkt.com/country/get/all";
+
 	public static void main(String[] args) {
 		log.debug("Hello world.");
+
+		try {
+
+			URL url = new URL(REST_URL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			log.debug("Fetched from server...");
+
+			while ((output = br.readLine()) != null) {
+				log.debug(output);
+
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
 
 	}
 
