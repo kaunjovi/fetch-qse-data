883640bb698ec02a961b18170f366e127f6bc81b
kaunjovi <kaunjovi@gmail.com>
2017-06-26 13:44:24 +0530

    generic class to call the actual workers.

---
+++ src/main/java/fun/n/games/HaveFun.java
@@ -0,0 +1,14 @@
+package fun.n.games;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HaveFun {
+
+	private static final Logger log = LoggerFactory.getLogger(HaveFun.class);
+
+	public static void main(String[] args) {
+		log.debug("Hello again.");
+
+	}
+}
