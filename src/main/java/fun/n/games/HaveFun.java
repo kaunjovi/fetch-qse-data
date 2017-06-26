3d75ffb4f6b7848cbc442a1d04fef993096f4677
kaunjovi <kaunjovi@gmail.com>
2017-06-26 13:52:41 +0530

    pull some raw data from 3rd party provider.

--- src/main/java/fun/n/games/HaveFun.java
+++ src/main/java/fun/n/games/HaveFun.java
@@ -10,5 +10,11 @@ public class HaveFun {
 	public static void main(String[] args) {
 		log.debug("Hello again.");
 
+		// TODO We should create the REST url from configuration, rather than
+		// having a static url.
+		String stockRestUrl = "http://finance.google.com/finance/info?client=ig&q=NSE:NIFTY,NSE:RELIANCE";
+
+		String stockDataRawString = RestService.getRestDataAsString(stockRestUrl);
+
 	}
 }
---
+++ src/main/java/fun/n/games/RestService.java
@@ -0,0 +1,16 @@
+package fun.n.games;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RestService {
+
+	private static final Logger log = LoggerFactory.getLogger(RestService.class);
+
+	public static String getRestDataAsString(String stockRestUrl) {
+		log.debug("Getting data from [{}].", stockRestUrl);
+
+		return null;
+	}
+
+}
